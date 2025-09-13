316e9fd00710fc61477dc86684cb3917df013479
Nanthakumar <35912811+NanthuDev@users.noreply.github.com>
2025-09-14 02:24:42 +0800

    Create controller.js

---
+++ modules/user/controller.js
@@ -0,0 +1,13 @@
+exports.getUsers = (req, res) => {
+  res.json([
+    { id: 1, name: "Nantha" },
+    { id: 2, name: "Kumar" },
+  ]);
+};
+
+exports.changeConfig = (req, res) => {
+  res.json([
+    { id: 1, name: "Nantha" },
+    { id: 2, name: "Kumar" },
+  ]);
+};
