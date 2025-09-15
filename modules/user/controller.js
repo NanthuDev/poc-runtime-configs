76df07f7c7dd74bab1b8c76dc2c0f3b115d64998
Nanthakumar <35912811+NanthuDev@users.noreply.github.com>
2025-09-15 23:17:40 +0800

    Update controller.js

--- modules/user/controller.js
+++ modules/user/controller.js
@@ -1,13 +1,16 @@
+const configProvider = require("../../global/config_switch");
+const dataLayer = require("../../global/data_layer");
+
 exports.getUsers = (req, res) => {
-  res.json([
-    { id: 1, name: "Nantha" },
-    { id: 2, name: "Kumar" },
-  ]);
+  //   const configs = configProvider.get("config1");
+  const get = dataLayer.getData();
+  res.json(get);
 };
 
 exports.changeConfig = (req, res) => {
-  res.json([
-    { id: 1, name: "Nantha" },
-    { id: 2, name: "Kumar" },
-  ]);
+  const setKey = configProvider.updateKey("config1");
+
+  const configs = configProvider.get("config1");
+
+  res.json(configs);
 };
