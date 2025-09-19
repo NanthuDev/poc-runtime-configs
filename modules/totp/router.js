6d5316cf5849ee53428fb3d88523b4d436f06f71
Nanthakumar <35912811+NanthuDev@users.noreply.github.com>
2025-09-20 00:31:13 +0800

    Update router.js

--- modules/totp/router.js
+++ modules/totp/router.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const totpController = require("./controller");
 
+router.get("/generate/secret", totpController.generateOtp);
+
 router.get("/generate", totpController.generateOtp);
 
 module.exports = router;
