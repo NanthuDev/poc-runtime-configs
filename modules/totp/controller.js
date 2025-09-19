9c3eb0d4d11abe52d0ce179f6b649f4206762681
Nanthakumar <35912811+NanthuDev@users.noreply.github.com>
2025-09-20 00:31:57 +0800

    Update controller.js

--- modules/totp/controller.js
+++ modules/totp/controller.js
@@ -1,4 +1,11 @@
-exports.generateOtp = (req, res) => {
+const speakeasy = require("speakeasy");
+
+exports.generateSecret = (req, res) => {
   //   const configs = configProvider.get("config1");
+  const secret = speak.generateSecret({
+    length: 20,
+    name: "TOTP Demo App",
+    issuer: "TOTP Demo",
+  });
   res.json("get");
 };
